bd665c5217e37592c67ee487562a2e2239b5e295
IMP1 <IMP1@mail.com>
2019-09-19 19:49:12 +0100

    Factor out drawing of person and draw everyone

--- _scripts/family_tree.js
+++ _scripts/family_tree.js
@@ -7,31 +7,61 @@ function createFamilyTree(me, parents, siblings, children) {
     let svg = document.getElementById("family-tree");
     svg.setAttribute("xmlns", SVGNS);
 
-    parents.forEach(function(p, i) {
-
-        let x = 80 + i * 60;
+    parents.forEach(function(person, i) {
+        let x = 120 + i * 120;
         let y = 80;
-
-        let group = document.createElementNS(SVGNS, "g");
-        group.setAttributeNS(null, "transform", "translate(" + x + " " + y + ")");
-
-        let name = document.createElementNS(SVGNS, "text");
-        name.setAttributeNS(null, "transform", "translate(0 0)");
-        name.setAttributeNS(null, "fill", "#000");
-        name.textContent = p.name;
-
-        let lifespan = document.createElementNS(SVGNS, "text");
-        lifespan.setAttributeNS(null, "transform", "translate(0 24)");
-        lifespan.setAttributeNS(null, "fill", "#000");
-        lifespan.textContent = yearFromBirthdate(p.birthdate);
-        if (p.deathdate) {
-            lifespan.textContent += " - " + yearFromBirthdate(p.deathdate);
-        }
-
-        group.appendChild(name);        
-        group.appendChild(lifespan);
-        svg.appendChild(group);        
+        drawFamilyMember(svg, person, x, y);
+            
     });
+    // TODO: draw line between parents
+    // TODO: draw line from parents to siblings
+    let meX = 0;
+    {
+        let thisGenerationIndex = 0;
+        youngerSiblings.forEach(function(person, i) {
+            let x = 60 + i * 120;
+            let y = 160;
+            drawFamilyMember(svg, person, x, y);
+            thisGenerationIndex = i;
+        });
+        thisGenerationIndex += 1;
+        meX = 60 + thisGenerationIndex * 120;
+        drawFamilyMember(svg, me, 60 + thisGenerationIndex * 120, 160);
+        thisGenerationIndex += 1;
+        olderSiblings.forEach(function(person, i) {
+            let x = 60 + (thisGenerationIndex + i) * 120;
+            let y = 160;
+            drawFamilyMember(svg, person, x, y);
+        });
+    }
+    // TODO: draw line from me to children
+    children.forEach(function(person, i) {
+        let x = 120 + i * 120;
+        let y = 240;
+        drawFamilyMember(svg, person, x, y);
+    });
+}
+
+function drawFamilyMember(svg, person, x, y) {
+    let group = document.createElementNS(SVGNS, "g");
+    group.setAttributeNS(null, "transform", "translate(" + x + " " + y + ")");
+
+    let name = document.createElementNS(SVGNS, "text");
+    name.setAttributeNS(null, "transform", "translate(0 0)");
+    name.setAttributeNS(null, "fill", "#000");
+    name.textContent = person.name;
+
+    let lifespan = document.createElementNS(SVGNS, "text");
+    lifespan.setAttributeNS(null, "transform", "translate(0 24)");
+    lifespan.setAttributeNS(null, "fill", "#000");
+    lifespan.textContent = yearFromBirthdate(person.birthdate);
+    if (person.deathdate) {
+        lifespan.textContent += " - " + yearFromBirthdate(person.deathdate);
+    }
+
+    group.appendChild(name);        
+    group.appendChild(lifespan);
+    svg.appendChild(group);    
 }
 
 function yearFromBirthdate(birthdate) {
