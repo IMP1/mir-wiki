74ca69f33ab9c7951e8cbab9c962c9a42a10da00
IMP1 <IMP1@mail.com>
2019-09-19 19:37:36 +0100

    Add positioning to elements

--- _scripts/family_tree.js
+++ _scripts/family_tree.js
@@ -7,21 +7,21 @@ function createFamilyTree(me, parents, siblings, children) {
     let svg = document.getElementById("family-tree");
     svg.setAttribute("xmlns", SVGNS);
 
-    parents.forEach(function(p) {
-        let group = document.createElementNS(SVGNS, "g");
+    parents.forEach(function(p, i) {
 
-        let x = 20;
-        let y = 20;
+        let x = 80 + i * 60;
+        let y = 80;
+
+        let group = document.createElementNS(SVGNS, "g");
+        group.setAttributeNS(null, "transform", "translate(" + x + " " + y + ")");
 
         let name = document.createElementNS(SVGNS, "text");
-        name.setAttributeNS(null, "x", x);
-        name.setAttributeNS(null, "y", y);
+        name.setAttributeNS(null, "transform", "translate(0 0)");
         name.setAttributeNS(null, "fill", "#000");
         name.textContent = p.name;
 
         let lifespan = document.createElementNS(SVGNS, "text");
-        lifespan.setAttributeNS(null, "x", x);
-        lifespan.setAttributeNS(null, "y", y + 24);
+        lifespan.setAttributeNS(null, "transform", "translate(0 24)");
         lifespan.setAttributeNS(null, "fill", "#000");
         lifespan.textContent = yearFromBirthdate(p.birthdate);
         if (p.deathdate) {
