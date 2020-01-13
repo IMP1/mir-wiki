e06399b7f11b25ac754c2823dbbcbf092891f678
IMP1 <IMP1@mail.com>
2020-01-13 15:59:01 +0000

    Add biology/fauna/cephalopods.html

---
+++ generate.sh
@@ -0,0 +1,3 @@
+cd /c/Users/huwtaylor/Projects/site_generation
+ruby generate --source /c/Users/huwtaylor/Projects/mir/wiki --source-branch content --target /c/Users/huwtaylor/Projects/mir/wiki --target-branch master
+cd /c/Users/huwtaylor/Projects/mir/wiki
