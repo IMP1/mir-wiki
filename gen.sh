9095d590eadea840c94d0ad6b1844fea06c28c5a
IMP1 <IMP1@mail.com>
2020-02-03 12:21:08 +0000

    Return to directory the script is called from

--- gen.sh
+++ gen.sh
@@ -1,3 +1,8 @@
+PREV_DIR=$(pwd)
 cd /c/Users/huwtaylor/Projects/site_generation
-ruby generate --source /c/Users/huwtaylor/Projects/mir/wiki --source-branch content --target /c/Users/huwtaylor/Projects/mir/wiki --target-branch master
-cd /c/Users/huwtaylor/Projects/mir/wiki
+ruby generate \
+ --source /c/Users/huwtaylor/Projects/mir/wiki \
+ --source-branch content \
+ --target /c/Users/huwtaylor/Projects/mir/wiki \
+ --target-branch master
+cd $PREV_DIR
